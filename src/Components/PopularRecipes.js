d14426a77c6b98fe6e26e60247a57b22ff068719
bakedegg <meetguleria94@gmail.com>
2022-01-24 21:39:56 -0800

    2

--- src/Components/PopularRecipes.js
+++ src/Components/PopularRecipes.js
@@ -1,8 +1,19 @@
-import React, {useState, useEffect} from 'react'
+import axios from "axios"
 
-const defaulturl = `https://api.edamam.com/api/recipes/v2?type=public&q=popular&app_id=b1bc7b45&app_key=570c93bce90c9f4f1540df521530a3b4`
+export default function PopularRecipes(){
 
-const [defaultRecipe, setDefaultRecipe] = useState("popular")
+    const defaulturl = `https://api.edamam.com/api/recipes/v2?type=public&q=popular&app_id=b1bc7b45&app_key=570c93bce90c9f4f1540df521530a3b4`
+
+    function fetchUrl() {
+        axios.get(defaulturl)
+        
+    }
+    return (
+        <div>
+        </div>
+    )
+
+}
 
 
 
